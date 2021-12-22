15a09d1c6b15885629e57b9b927910286b965491
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2021-12-22 14:11:14 +0600

    added example5

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ var example1 = 10 === 10; //equals
 var example2 = 11 > 10; // more than
 var example3 = 10 < 11; //less than
 var example4 = 10 >= 10; //more than or equal
- 
+var example5 = 10 <= 10; //less than or equal
