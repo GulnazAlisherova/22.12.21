2cb8d1eae9428577b419f6456242dc34bed094be
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2021-12-22 15:32:49 +0600

    added apple pears

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
-var example1 = 10 === 10; //equals
-var example2 = 11 > 10; // more than
-var example3 = 10 < 11; //less than
-var example4 = 10 >= 10; //more than or equal
-var example5 = 10 <= 10; //less than or equal
-var example6 = 10 !== 10; //not equals 
+// var example1 = 10 === 10; //equals
+// var example2 = 11 > 10; // more than
+// var example3 = 10 < 11; //less than
+// var example4 = 10 >= 10; //more than or equal
+// var example5 = 10 <= 10; //less than or equal
+// var example6 = 10 !== 10; //not equals 
 
 
-var a = 5;
-var b = 10;
-var exmaple7 = a < b ;
+// var a = 5;
+// var b = 10;
+// var exmaple7 = a < b ;
 
 
-var c = 12;
-var d = 11;
-if (c < d) { //true
-  alert("C is less than D");
-  console.log ('Hello world')
-}
+// var c = 12;
+// var d = 11;
+// if (c < d) { //true
+//   alert("C is less than D");
+//   console.log ('Hello world')
+// }
 
 
-var e = 3.14;
-if (e !== 3.14 ) {
-  alert("Why don't you know numver PI");
+// var e = 3.14;
+// if (e !== 3.14 ) {
+//   alert("Why don't you know numver PI");
+// }
+// else {
+//   alert("hi bye!")
+// }
+
+// var age = 10 ;
+// if (age >= 18) {
+//   alert("do yuo have a car"); //true
+// }
+// else {
+//   alert("you are stupid!"); //false
+// }
+
+
+
+var apple = 5
+var pear = 6
+if (apple < pear) {
+  alert("pears are won");
 }
 else {
-  alert("Good job!")
-}
-
-var age = 10 ;
-if (age >= 18) {
-  alert("You can vote!");
+  alert("apples are won");
 }
